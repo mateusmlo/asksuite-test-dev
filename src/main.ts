515e56da1049b8fa8a40d42923bfeae956c01247
Mateus Mendonça <mateus.mlo95@gmail.com>
2023-12-15 16:41:15 -0300

    chore: add necessary libs and setup

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('MAIN');
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('v1');
+
   await app.listen(process.env.APP_PORT, async () => {
-    logger.log(`App running at ${await app.getUrl()}`);
+    logger.log(`⭐️ App running at ${await app.getUrl()}`);
   });
 }
 
